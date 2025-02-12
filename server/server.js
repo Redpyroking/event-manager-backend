729b8b417e0720ab185d89eeda2bccd4d1b4ce4d
Nihal Shrivastav <nihalrs24@gmail.com>
2025-02-12 21:17:31 +0530

    complete 1.12

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ function verifyToken(req, res, next) {
 
   try {
       const decoded = jwt.verify(token, SECRET_KEY);
-      req.user = decoded;
+      req.user = decoded.user;
       next();
   } catch (err) {
       res.status(401).json({ message: 'Invalid token' });
